d8f6faf080df38eb44e41ec41485d752f49f01c4
Ruben Galoyan <ruben071099@gmail.com>
2020-05-15 11:29:57 +0300

    fix

--- src/sql/chat.sql
+++ src/sql/chat.sql
@@ -1,13 +1,7 @@
-CREATE TABLE chat_?.`chat`
-(
-    id varchar
-(16) PRIMARY KEY ,
+CREATE TABLE chat_?.`chat`(
+    id varchar(16) PRIMARY KEY,
     create_time TIMESTAMP NOT NULL,
     expiration_time TIMESTAMP NULL DEFAULT NULL,
-    identifier varchar
-(16) NOT NULL,
-    password_salt varchar
-(16) DEFAULT NULL,
-    password_hash varchar
-(24) DEFAULT NULL
+    password_salt varchar(16) DEFAULT NULL,
+    password_hash varchar(24) DEFAULT NULL
 );
